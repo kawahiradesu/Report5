dbbafbd14edf635fd8bb027677814a1212263054
FUMA <e245717@ie.u-ryukyu.ac.jp>
2024-12-16 12:46:36 +0900

    step2 at rep5

--- src/Report5.java
+++ src/Report5.java
@@ -1,8 +1,12 @@
 public class Report5 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String srt = null;
+            System.out.println(srt.length());
+        } catch (NullPointerException e) {
+            // TODO: handle exception
+            System.out.println("nullでエラー吐いてる");
+            System.out.println(e.getMessage());
+        }
     }
-    
 }
-    
